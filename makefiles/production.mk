7a269ed9aceb35758bf14e2230c710f065ce3425
Laszlo Losonczy <Laszlo.Losonczy@us.bosch.com>
2020-05-21 16:22:21 -0400

    refactor makefile

---
+++ makefiles/production.mk
@@ -0,0 +1,13 @@
+
+sources = ${wildcard src/*.cpp}
+sourceObjects = $(patsubst src/%.cpp, $(buildDir)/%.o, $(sources))
+sourceDependencies = $(patsubst src/%.cpp, $(buildDir)/%.d, $(sources))
+
+-include $(sourceDependencies)
+
+$(buildDir)/%.o: src/%.cpp
+	mkdir -p $(buildDir)
+	g++ -c -o $@ $(CXXFLAGS) $< -Iinclude $(createDependencies)
+
+$(buildDir)/main: main.cpp $(sourceObjects)
+	g++ -o $@ $(CXXFLAGS) -Iinclude $^
---
+++ makefiles/test.mk
@@ -0,0 +1,22 @@
+GTEST_INC = googletest/googletest/include
+GTEST_LIB = $(libDir)/gtest/lib
+GTEST_LIBS = $(wildcard $(libDir)/gtest/lib/*.a)
+
+tests = ${wildcard test/*.cpp}
+testsObjects = $(patsubst test/%.cpp, $(buildDir)/%.o, $(tests))
+testsDependencies = $(patsubst test/%.cpp, $(buildDir)/%.d, $(tests))
+
+$(libDir)/gtest/lib/libgtest.a:
+	mkdir -p $(libDir)/gtest
+	cd $(libDir)/gtest && cmake ../../googletest/
+	cd $(libDir)/gtest && make
+
+-include $(testsDependencies)
+
+$(buildDir)/%.o: test/%.cpp
+	mkdir -p $(buildDir)
+	g++ -c -o $@ $(CXXFLAGS) $< -I$(GTEST_INC) -Iinclude $(createDependencies)
+
+$(buildDir)/testmain: $(testsObjects) $(GTEST_LIBS) $(sourceObjects) $(libDir)/gtest/lib/libgtest.a
+	mkdir -p $(buildDir)
+	g++ -o $@ $(CXXFLAGS) -L./$(GTEST_LIB) -lgtest_main -lgtest $(testsObjects) $(sourceObjects) -I$(GTEST_INC)
