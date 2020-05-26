fbe2fbbf52d1b249a8fc55d802c15bde303dd9c9
Laszlo Losonczy <losonczylaci@gmail.com>
2020-05-26 09:25:35 -0400

    move semantics example

--- include/rectangle.hpp
+++ include/rectangle.hpp
@@ -1,12 +1,36 @@
-#pragma once
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include <cstdio>
+#include <utility>
 
 class Rectangle {
-
-public:
+   public:
     Rectangle(int a, int b) : m_a(a), m_b(b) {}
 
+    Rectangle(const Rectangle& lhs) = default;
+    Rectangle& operator= (const Rectangle& rhs) = default;
+    
+    Rectangle(Rectangle&& rhs) noexcept {
+        puts("move constructor");
+        m_a = rhs.m_a; //trivial type, no need to cal std::move expliclitly
+        m_b = rhs.m_b;
+    }
+
+    Rectangle& operator= (Rectangle&& rhs) noexcept {
+        puts("move assignment operator");
+        m_a = rhs.m_a; //trivial type, no need to call std::move explicitly
+        m_b = rhs.m_b;
+        return *this;
+    }
+
+    ~Rectangle() = default;
+
     int getArea();
-private:
+
+   private:
     int m_a;
     int m_b;
 };
+
+#endif
