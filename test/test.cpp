22bfd3e7a71e1fe6ff7dad881d4953a0d2354bc8
Laszlo Losonczy <Laszlo.Losonczy@us.bosch.com>
2020-05-26 09:25:35 -0400

    move semantics example

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,21 @@
-
+#include <utility>
 #include <gtest/gtest.h>
 #include <rectangle.hpp>
 
+using namespace std;
+
+
 TEST(test, test1) {
     Rectangle rect(1,2);
+    Rectangle rectB = rect;
+    ASSERT_EQ(2, rect.getArea());
+    ASSERT_EQ(2, rectB.getArea());
+
+    //move assignment
+    rect = std::move(rectB);
+    ASSERT_EQ(2, rect.getArea());
+    
+    // move contructor
+    Rectangle movetor = move(rectB);
     ASSERT_EQ(2, rect.getArea());
 };
