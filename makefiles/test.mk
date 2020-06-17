e2be3f010fc9ff7478608116af7da189abd8700f
Laszlo Losonczy <losonczylaci@gmail.com>
2020-06-16 20:36:06 -0400

    add gmock to the build

--- makefiles/test.mk
+++ makefiles/test.mk
@@ -1,4 +1,5 @@
 GTEST_INC = googletest/googletest/include
+GMOCK_INC = googletest/googlemock/include
 GTEST_LIB = $(libDir)/gtest/lib
 GTEST_LIBS = $(wildcard $(libDir)/gtest/lib/*.a)
 
@@ -15,8 +16,8 @@ $(libDir)/gtest/lib/libgtest.a:
 
 $(buildDir)/%.o: test/%.cpp
 	mkdir -p $(buildDir)
-	g++ -c -o $@ $(CXXFLAGS) $< -I$(GTEST_INC) -Iinclude $(createDependencies)
+	g++ -c -o $@ $(CXXFLAGS) $< -I$(GTEST_INC) -I$(GMOCK_INC) -Iinclude $(createDependencies) 
 
 $(buildDir)/testmain: $(testsObjects) $(GTEST_LIBS) $(sourceObjects) $(libDir)/gtest/lib/libgtest.a
 	mkdir -p $(buildDir)
-	g++ -o $@ $(CXXFLAGS) -L./$(GTEST_LIB) -lgtest_main -lgtest $(testsObjects) $(sourceObjects) -I$(GTEST_INC)
+	g++ -o $@ $(CXXFLAGS) -L./$(GTEST_LIB) -lgmock_main -lgmock -lgtest_main -lgtest $(testsObjects) $(sourceObjects) -I$(GTEST_INC) -I$(GMOCK_INC)
