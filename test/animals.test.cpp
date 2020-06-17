da0921dfc15cbd1477dca650625a810ed4b0551c
Laszlo Losonczy <losonczylaci@gmail.com>
2020-06-16 21:06:20 -0400

    fixed clang-tidy warnings

--- test/animals.test.cpp
+++ test/animals.test.cpp
@@ -5,7 +5,7 @@
 
 class DogMock : public Dog {
    public:
-    DogMock(const std::string name) : Dog(name) {}
+    DogMock(const std::string& name) : Dog(name) {}
     MOCK_CONST_METHOD0(saySomething, void());
 };
 
