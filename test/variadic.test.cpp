a3566bf5c0b060383184a9e0c57130011280ee61
Laszlo Losonczy <losonczylaci@gmail.com>
2020-06-27 11:41:16 -0400

    variadic template function exercise

--- test/variadic.test.cpp
+++ test/variadic.test.cpp
@@ -15,3 +15,17 @@ TEST(variadicGroup, reverseCatenate) {
     std::string a = "a", b = "b";
     EXPECT_EQ("b a", rcatenate(a, b));
 }
+
+template <typename T>
+T summer(T first) {
+    return first;
+}
+template <typename T, typename... Args>
+T summer(T first, Args... args) {
+    return first + summer(args...);
+}
+
+TEST(variadicGroup, intSum) {
+    int a = 1, b = 2, c = 3;
+    EXPECT_EQ(6, summer(a, b, c));
+}
